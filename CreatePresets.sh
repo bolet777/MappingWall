9efb01d8426d61eb8bc6a199be1d5bfbc582cf9f
bolet777 <cyril.cosenza@gmail.com>
2018-05-16 10:58:16 -0400

    Add files via upload

---
+++ CreatePresets.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+INPUT_FILE="ofxpimapper.xml"
+OUTPUT_FILE="ofxpimapper"
+
+TOKEN1="__IMAGE__.jpg"
+TOKEN2="__NAME___.jpg"
+
+length=12
+
+
+for i in $( seq 1 $length )
+do
+	echo "1) Create file "$OUT
+	OUT=$OUTPUT_FILE.$i.xml
+	cp $INPUT_FILE $OUT
+
+	echo "2) Copy calibration value in "$OUT
+	sed -- "s/$TOKEN1/image$i.jpg/g" $INPUT_FILE | sed -- "s/$TOKEN2/nom$i.jpg/g" > $OUT
+
+done
